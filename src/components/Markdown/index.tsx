ed9ec81ff8854509af4b0b4163e52ce600bb4622
chenc <chenchen3@fosun.com>
2021-05-22 18:25:56 +0800

    feat: markdown组件

---
+++ src/components/Markdown/index.tsx
@@ -0,0 +1,16 @@
+import 'bytemd/dist/index.min.css';
+import { Editor } from '@bytemd/react';
+import gfm from '@bytemd/plugin-gfm';
+
+const plugins = [
+  gfm(),
+  // Add more plugins here
+];
+
+const Markdown: ({ value, onChange }: { value?: any; onChange?: any }) => JSX.Element = ({
+  value = '',
+  onChange,
+}): JSX.Element => {
+  return <Editor value={value} onChange={(val) => onChange(val)} plugins={plugins} />;
+};
+export default Markdown;
