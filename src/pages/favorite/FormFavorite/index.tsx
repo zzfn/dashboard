44761ceb96622fc159912cd930b9b67a88c906dc
chenc <git@zzfzzf.com>
2021-05-23 15:33:54 +0800

    feat: 收藏管理

---
+++ src/pages/favorite/FormFavorite/index.tsx
@@ -0,0 +1,164 @@
+import { Button, Card, Form, Input, message, Space } from 'antd';
+import type { Dispatch } from 'dva';
+import { useLocation, history } from 'umi';
+import type { FC } from 'react';
+import { useEffect, useState } from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { saveFavorite, fetchFavorite } from '@/pages/favorite/FormFavorite/service';
+import SelectCode from '@/components/SelectCode';
+
+const FormItem = Form.Item;
+
+type FormFavoriteProps = {
+  submitting: boolean;
+  dispatch: Dispatch;
+};
+
+const FormFavorite: FC<FormFavoriteProps> = (props) => {
+  const { submitting } = props;
+  const [form] = Form.useForm();
+  const location = useLocation();
+
+  const { id } = (location as any).query;
+  const [isRelease, setIsRelease] = useState(false);
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 2 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 22 },
+      md: { span: 22 },
+    },
+  };
+
+  const submitFormLayout = {
+    wrapperCol: {
+      xs: { span: 24, offset: 11 },
+      sm: { span: 10, offset: 11 },
+    },
+  };
+
+  const onFinish = async (values: Record<string, any>): Promise<void> => {
+    const { data } = await saveFavorite({ ...values, isRelease, id });
+    data && history.replace({ pathname: 'form-favorite', query: { id: data as string } });
+  };
+
+  const handleFetchArticle = async () => {
+    const { data, msg } = await fetchFavorite({ id });
+    if (msg === 'success') {
+      form.setFieldsValue(data);
+    } else {
+      message.error('未知错误');
+    }
+  };
+  useEffect(() => {
+    id && handleFetchArticle().then();
+  }, [id]);
+  return (
+    <PageContainer>
+      <Card bordered={false}>
+        <Form
+          {...formItemLayout}
+          hideRequiredMark
+          style={{ marginTop: 8 }}
+          form={form}
+          name="basic"
+          initialValues={{ public: '1' }}
+          onFinish={onFinish}
+        >
+          <FormItem
+            label={'标题'}
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: '88',
+              },
+            ]}
+          >
+            <Input placeholder={'请输入标题'} />
+          </FormItem>
+          <FormItem
+            label={'链接'}
+            name="link"
+            rules={[
+              {
+                required: true,
+                message: '请输入链接',
+              },
+            ]}
+          >
+            <Input placeholder={'请输入链接'} />
+          </FormItem>
+          <FormItem
+            label={'分类'}
+            name="category"
+            rules={[
+              {
+                required: true,
+                message: '请选择分类',
+              },
+            ]}
+          >
+            <SelectCode placeholder={'请选择分类'} code={'FAVORITE'} />
+          </FormItem>
+          <FormItem
+            label={'图片'}
+            name="img"
+            rules={[
+              {
+                required: true,
+                message: '请输入图片',
+              },
+            ]}
+          >
+            <Input placeholder={'请输入图片'} />
+          </FormItem>
+          <FormItem
+            label={'排序号'}
+            name="orderNum"
+            rules={[
+              {
+                required: true,
+                message: '请输入排序号',
+              },
+            ]}
+          >
+            <Input placeholder={'请输入文章'} />
+          </FormItem>
+          <FormItem
+            label={'备注'}
+            name="remark"
+            rules={[
+              {
+                required: true,
+                message: '请输入备注',
+              },
+            ]}
+          >
+            <Input placeholder={'请输入备注'} />
+          </FormItem>
+          <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+            <Space>
+              <Button
+                onClick={() => setIsRelease(true)}
+                type="primary"
+                htmlType="submit"
+                loading={submitting}
+              >
+                发布
+              </Button>
+              <Button onClick={() => setIsRelease(false)} htmlType="submit">
+                保存
+              </Button>
+            </Space>
+          </FormItem>
+        </Form>
+      </Card>
+    </PageContainer>
+  );
+};
+
+export default FormFavorite;
---
+++ src/pages/favorite/ListFavorite/data.d.ts
@@ -0,0 +1,31 @@
+export type TableListItem = {
+  id: string;
+  title: string;
+  tag: string;
+  isRelease: boolean;
+  orderNum: number;
+  createTime: Date;
+  updateTime: Date;
+};
+
+export type TableListPagination = {
+  total: number;
+  pageSize: number;
+  current: number;
+};
+
+export type TableListData = {
+  list: TableListItem[];
+  pagination: Partial<TableListPagination>;
+};
+
+export type TableListParams = {
+  status?: string;
+  name?: string;
+  desc?: string;
+  key?: number;
+  pageSize?: number;
+  currentPage?: number;
+  filter?: Record<string, any[]>;
+  sorter?: Record<string, any>;
+};
