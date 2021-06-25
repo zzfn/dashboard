16ed0872e20921140a5c38829126ad9b399a5c2f
chenc <chenchen3@fosun.com>
2021-06-25 10:15:55 +0800

    feat: 添加测试用户

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -148,7 +148,7 @@ const Login: React.FC = () => {
               <LoginMessage
                 content={intl.formatMessage({
                   id: 'pages.login.accountLogin.errorMessage',
-                  defaultMessage: '账户或密码错误（admin/ant.design)',
+                  defaultMessage: '账户或密码错误（test/test)',
                 })}
               />
             )}
@@ -162,7 +162,7 @@ const Login: React.FC = () => {
                   }}
                   placeholder={intl.formatMessage({
                     id: 'pages.login.username.placeholder',
-                    defaultMessage: '用户名: admin or user',
+                    defaultMessage: '用户名: test',
                   })}
                   rules={[
                     {
@@ -184,7 +184,7 @@ const Login: React.FC = () => {
                   }}
                   placeholder={intl.formatMessage({
                     id: 'pages.login.password.placeholder',
-                    defaultMessage: '密码: ant.design',
+                    defaultMessage: '密码: test',
                   })}
                   rules={[
                     {
